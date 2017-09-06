9f2277d31063fa3c0f8b9ec8ede3d55cf20179ce
jackwang <wangxichao001@gmail.com>
2017-09-06 17:31:11 +0800

    change

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,5 +1,82 @@
 <template>
     <div id="app" v-cloak>
+
+        <f7-statusbar></f7-statusbar>
+        <!-- Left Panel with "cover" effect -->
+        <f7-panel left cover>
+            <f7-view>
+                <f7-pages navbar-fixed>
+                    <f7-page>
+                        <f7-navbar title="Left Panel"></f7-navbar>
+                        <f7-block>
+                            <p>Here comes the left panel text</p>
+                        </f7-block>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-panel>
+        <!-- Right Panel with "reveal" effect -->
+        <f7-panel right reveal>
+            <f7-view>
+                <f7-pages navbar-fixed>
+                    <f7-page>
+                        <f7-navbar title="Right Panel"></f7-navbar>
+                        <f7-block>
+                            <p>Here comes the right panel text</p>
+                        </f7-block>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-panel>
+        <!-- Views -->
+        <f7-views>
+            <!-- Main view-->
+            <f7-view main navbar-through toolbar-fixed>
+                <!-- For iOS theme we use navbar-through layout so the navbar should be a child of the View -->
+                <f7-navbar v-if="$theme.ios" title="Awesome App"></f7-navbar>
+                <f7-pages navbar-fixed toolbar-fixed>
+                    <f7-page>
+                        <!-- For Material theme we use navbar-fixed layout so the navbar should be a child of the Page -->
+                        <f7-navbar v-if="$theme.material" title="Awesome App"></f7-navbar>
+                        <!-- Page content -->
+                        <f7-block>
+                            <p>Here comes main view page text</p>
+                        </f7-block>
+                        <!-- Buttons to open panels -->
+                        <f7-grid>
+                            <f7-col>
+                                <f7-button open-panel="left">Left Panel</f7-button>
+                            </f7-col>
+                            <f7-col>
+                                <f7-button open-panel="right">Right Panel</f7-button>
+                            </f7-col>
+                        </f7-grid>
+                        <!-- Button to open popup -->
+                        <f7-button open-popup="#my-popup">Open Popup</f7-button>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-views>
+        <!-- Popup. All modals should be outside of Views -->
+        <f7-popup id="my-popup">
+            <f7-view>
+                <f7-pages navbar-fixed>
+                    <f7-page>
+                        <f7-navbar title="Popup">
+                            <!-- Link to close popup -->
+                            <f7-nav-right>
+                                <f7-link close-popup>Close</f7-link>
+                            </f7-nav-right>
+                        </f7-navbar>
+                        <f7-block>
+                            <p>Here comes popup text</p>
+                        </f7-block>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-popup>
+
+
         <h1>Apache Cordova</h1>
         <div id="deviceready" class="blink">
             <p class="event listening">Connecting to Device</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'framework7'
+import Framework7Vue from 'framework7-vue'
+import 'framework7/dist/css/framework7.css'
+import 'framework7/dist/css/framework7.ios.css'
+Vue.use(Framework7Vue)
+
 import App from './App'
-// import 'amfe-flexible'
 
 if (envConfig.environment === 'production') { // global config variable
     Vue.config.productionTip = false
@@ -26,6 +31,10 @@ if (window.location.protocol === 'file:' || window.location.port === '3000') {
 new Vue({
     el: '#app',
     render: h => h(App),
+    framework7: {
+        root: '#app',
+        routes: []
+    },
     data: function () {
         return {
             cordova: Vue.cordova
